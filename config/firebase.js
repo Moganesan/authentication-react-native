9c8cce886ae0f470f7a08eba53632eafe10191d9
Moganesan <43741383+Moganesan@users.noreply.github.com>
2021-11-27 14:17:18 -0500

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -7,12 +7,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAF512VXQt0Si1YbDmsJuQKDKGeQWBtSqc",
-  authDomain: "authentication-system-df778.firebaseapp.com",
-  projectId: "authentication-system-df778",
-  storageBucket: "authentication-system-df778.appspot.com",
-  messagingSenderId: "899848361501",
-  appId: "1:899848361501:web:a01a846f2466d7e01158f4",
+  apiKey: "#######################################",
+  authDomain: "#######################################",
+  projectId: "#######################################",
+  storageBucket: "#######################################",
+  messagingSenderId: "#######################################",
+  appId: "#######################################",
 };
 
 initializeApp(firebaseConfig);
